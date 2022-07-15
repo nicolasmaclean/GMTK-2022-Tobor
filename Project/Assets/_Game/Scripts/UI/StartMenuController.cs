37aab337662f3f178b1ada6ba8fe7b67304d680b
Nick Maclean <nicolasimaclean@gmail.com>
2022-07-15 16:10:28 -0500

    connect start to character

---
+++ Project/Assets/_Game/Scripts/UI/StartMenuController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.Core;
+
+namespace Game.UI
+{
+    public class StartMenuController : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneController.Instance.LoadNextScene();
+        }
+
+        public void Quit()
+        {
+            SceneController.Instance.Quit();
+        }
+    }
+}
