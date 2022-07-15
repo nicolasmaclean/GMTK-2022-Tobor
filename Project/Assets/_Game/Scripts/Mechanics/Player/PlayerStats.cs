900069a8fcd611ad11e58be30f7e8323020b7551
Nick Maclean <nicolasimaclean@gmail.com>
2022-07-15 18:12:21 -0500

    setting up PlayerStats and connecting to FPSController

---
+++ Project/Assets/_Game/Scripts/Mechanics/Player/PlayerStats.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Game.Mechanics.Player
+{
+    [System.Serializable]
+    public class PlayerStats
+    {
+        [Range(1, 20)]
+        public int Constitution = 1;
+
+        [Range(1, 20)]
+        public int Strength = 1;
+
+        [Range(1, 20)]
+        public int Agility = 1;
+
+        [Range(1, 20)]
+        public int Luck = 1;
+
+        [Range(1, 20)]
+        public int Perception = 1;
+
+        public WeaponType Weapon = WeaponType.Sword;
+    }
+
+}
+
+public enum WeaponType
+{
+    Sword = 0, Bow = 1, Magic = 2
+}
