d00641eaf738b1264913bc1d9fce47fba17be3ab
Nick Maclean <nicolasimaclean@gmail.com>
2022-07-17 04:31:33 -0500

    centralized modifers

--- Project/Assets/_Game/Scripts/Mechanics/Level/DiceTower.cs
+++ Project/Assets/_Game/Scripts/Mechanics/Level/DiceTower.cs
@@ -34,7 +34,7 @@ namespace Game.Mechanics.Level
 
         void Roll(int numberOfDice)
         {
-            List<int> rolls = new List<int>();
+            int[] rolls = new int[6] { 0, 0, 0, 0, 0, 0 };
             
             for (int i = 0; i < _dice.Length; i++)
             {
@@ -42,7 +42,7 @@ namespace Game.Mechanics.Level
                 if (i < numberOfDice)
                 {
                     int roll = UnityEngine.Random.Range(1, 6);
-                    rolls.Add(roll);
+                    rolls[roll-1]++;
                     
                     go.SetActive(true);
                     ShowDie(go, roll);
@@ -54,6 +54,7 @@ namespace Game.Mechanics.Level
             }
             
             _animator.SetTrigger(AT_Roll);
+            Modifiers.SetMultipliers(rolls[0], rolls[1], rolls[2], rolls[3], rolls[4], rolls[5]);
             StartCoroutine(WaitThen(3.3f, () =>
             {
                 LevelController.CurrentRoom.Done();
