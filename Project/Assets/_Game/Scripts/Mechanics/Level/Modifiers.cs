0f60dd105b590510e12f9aa394fc7f7b2b913403
Nick Maclean <nicolasimaclean@gmail.com>
2022-07-17 04:28:14 -0500

    centralized modifers

---
+++ Project/Assets/_Game/Scripts/Mechanics/Level/Modifiers.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Modifiers
+{
+    public static int EnemyMultiplier { get; private set; } = 1;
+    public static int DamageMultiplier { get; private set; } = 1;
+    public static int SpeedMultiplier { get; private set; } = 1;
+    public static int AttackSpeedMultiplier { get; private set; } = 1;
+    public static int JumpMultiplier { get; private set; } = 1;
+    public static int BaseStatsMultiplier { get; private set; } = 1;
+}
