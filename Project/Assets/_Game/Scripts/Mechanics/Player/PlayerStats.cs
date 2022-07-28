5b637aee37d517943959f573d27e4251a1c722b3
Nick Maclean <nicolasimaclean@gmail.com>
2022-07-28 13:15:21 -0500

    🚀🚀🚀 modifiers!

--- Project/Assets/_Game/Scripts/Mechanics/Player/PlayerStats.cs
+++ Project/Assets/_Game/Scripts/Mechanics/Player/PlayerStats.cs
@@ -34,7 +34,7 @@ namespace Game.Mechanics.Player
 
         [Range(1, 20)]
         public int Agility = 1;
-        public Vector2 AgilityRange = new Vector2(8, 24);
+        public Vector2 AgilityRange = new Vector2(10, 20);
 
         [Range(1, 20)]
         public int Luck = 1;
